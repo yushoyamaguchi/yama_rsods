a2889cb0498dd65d51f1cc7b0a863f7bac8a7c0c
yushoyamaguchi <ysh.824@outlook.jp>
2023-11-04 12:12:01 +0900

    immutable error

--- chap5/src/samplehashtable.rs
+++ chap5/src/samplehashtable.rs
@@ -2,7 +2,7 @@ use std::collections::LinkedList;
 const INITIAL_SIZE: usize = 128;
 
 
-#[derive(Hash, Eq, PartialEq, Clone, Debug)]
+#[derive( Eq, PartialEq, Clone, Debug)]
 struct Entry<K, V> {
     key: K,
     value: V,
@@ -10,4 +10,34 @@ struct Entry<K, V> {
 
 struct ChainedHashMap<K, V> {
     buckets: Vec<LinkedList<Entry<K, V>>>,
+}
+
+impl<K,V> ChainedHashMap<K,V>
+where
+    K: Eq,
+    V: Clone,
+{
+    fn new() -> ChainedHashMap<K,V>{
+        let mut buckets = Vec::with_capacity(INITIAL_SIZE);
+        for _ in 0..INITIAL_SIZE {
+            buckets.push(LinkedList::new());
+        }
+        ChainedHashMap { buckets }
+    }
+    fn hash(&self,key:&K) -> usize {
+        0
+    }
+
+    fn insert(&self, key: K, value: V) -> Option<V> {
+        let hash = self.hash(&key);
+        let bucket = &self.buckets[hash];
+        for entry in bucket {
+            if entry.key == key {
+                let old_value = entry.value.clone();
+                entry.value = value;
+                return Some(old_value);
+            }
+        }
+        None
+    }
 }
